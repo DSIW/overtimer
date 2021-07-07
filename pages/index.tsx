3d2c571d732818ac40fd7f8b4898e7c9241160c2
DSIW <dsiw@dsiw-it.de>
2021-07-07 09:11:36 +0200

    Update favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ export default function Home() {
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
         <meta name="" content="Overtimer" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB"/>
