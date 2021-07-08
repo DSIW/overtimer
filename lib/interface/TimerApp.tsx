cb125bd164ac549c5c6afe2f31cf4b5ac8091898
DSIW <dsiw@dsiw-it.de>
2021-07-08 15:23:30 +0200

    Remove unused import

--- lib/interface/TimerApp.tsx
+++ lib/interface/TimerApp.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
-import { formatDuration } from 'date-fns'
 import { useStopwatch } from 'react-timer-hook'
 import TimeLog from '../domain/TimeLog'
 import TimeLogTable from './TimeLogTable'
