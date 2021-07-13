0bf4514532b3ebd0aa8d69a2c35fae5aefef2bc5
DSIW <dsiw@dsiw-it.de>
2021-07-13 16:39:17 +0200

    Remove sentry dsn credentials

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://b663b434a49c4c72a04863450ebb3b4a@o917625.ingest.sentry.io/5860227',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
