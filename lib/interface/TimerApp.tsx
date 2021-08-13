791980dfea987a91aedfa85376a92e3db917efa0
DSIW <dsiw@dsiw-it.de>
2021-08-13 10:03:04 +0200

    Move handleAction to TimeLogsTable

--- lib/interface/TimerApp.tsx
+++ lib/interface/TimerApp.tsx
@@ -3,7 +3,6 @@ import TimeLog from "../domain/TimeLog";
 import TimeLogTable from "./table/TimeLogTable";
 import TimeLogSummary from "./stats/TimeLogSummary";
 import { useLiveQuery } from "dexie-react-hooks";
-import { Action } from "./table/TableRowActionButton";
 import TimerContainer from "./timer/TimerContainer";
 import { timerApplicationService } from "../application/TimerApplicationService";
 import { SnackbarProvider } from "notistack";
@@ -23,24 +22,13 @@ export default function TimerApp() {
 
   useWindowFocus();
 
-  async function handleAction(action: Action, timeLog: TimeLog) {
-    switch (action) {
-      case "delete":
-        await timerApplicationService.delete(timeLog);
-        return;
-      case "edit":
-        await timerApplicationService.update(timeLog);
-        return;
-    }
-  }
-
   return (
     <>
       <SnackbarProvider maxSnack={1}>
         <TimerContainer timeLogs={timeLogs} />
         <TimeLogSummary timeLogs={timeLogs} />
         <PersistenceWarning timeLogs={timeLogs} />
-        <TimeLogTable timeLogs={timeLogs} onAction={handleAction} />
+        <TimeLogTable timeLogs={timeLogs} />
       </SnackbarProvider>
     </>
   );
