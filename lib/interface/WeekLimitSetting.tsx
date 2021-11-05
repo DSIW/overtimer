52e443e1cd951390c197c8a989ce4d6a61e5ccf1
DSIW <dsiw@dsiw-it.de>
2021-11-05 11:20:24 +0100

    Use styled FormControl

--- lib/interface/WeekLimitSetting.tsx
+++ lib/interface/WeekLimitSetting.tsx
@@ -1,10 +1,6 @@
-import {
-  FormControlLabel,
-  FormGroup,
-  MenuItem,
-  Select,
-} from "@material-ui/core";
+import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
 import { ChangeEvent } from "react";
+import styles from "./WeekLimitSetting.module.css";
 
 interface Props {
   weekLimit: number;
@@ -18,27 +14,22 @@ export default function WeekLimitSetting({ weekLimit, onChange }: Props) {
 
   return (
     <div>
-      <FormGroup row>
-        <FormControlLabel
-          control={
-            <Select
-              labelId="week-limmit"
-              id="week-limit"
-              value={weekLimit}
-              onChange={handleChange}
-            >
-              <MenuItem value={1}>1 Woche</MenuItem>
-              <MenuItem value={1 * 4}>1 Monat</MenuItem>
-              <MenuItem value={2 * 4}>2 Monate</MenuItem>
-              <MenuItem value={4 * 4}>4 Monate</MenuItem>
-              <MenuItem value={6 * 4}>6 Monate</MenuItem>
-              <MenuItem value={52}>1 Jahr</MenuItem>
-            </Select>
-          }
-          label={`Limit times logs to `}
-          labelPlacement="start"
-        />
-      </FormGroup>
+      <FormControl className={styles.formControl}>
+        <InputLabel id="week-limit-select-label">Time log limit</InputLabel>
+        <Select
+          labelId="week-limit-select-label"
+          id="week-limit-select"
+          value={weekLimit}
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>1 Woche</MenuItem>
+          <MenuItem value={1 * 4}>1 Monat</MenuItem>
+          <MenuItem value={2 * 4}>2 Monate</MenuItem>
+          <MenuItem value={4 * 4}>4 Monate</MenuItem>
+          <MenuItem value={6 * 4}>6 Monate</MenuItem>
+          <MenuItem value={52}>1 Jahr</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
