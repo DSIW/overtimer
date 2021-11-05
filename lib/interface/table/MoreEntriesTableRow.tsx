fe08947f5d106b786460a2f49b5a16f6ccbfad39
DSIW <dsiw@dsiw-it.de>
2021-11-05 11:12:24 +0100

    Pluralize more entries sentence

--- lib/interface/table/MoreEntriesTableRow.tsx
+++ lib/interface/table/MoreEntriesTableRow.tsx
@@ -5,10 +5,16 @@ interface Props {
   weekLimit: number;
 }
 
+function pluralize(value: number, singular: string, plural?: string) {
+  const pluralValue = plural || `${singular}s`;
+  return value === 1 ? singular : pluralValue;
+}
+
 export default function MoreEntriesTableRow({ count, weekLimit }: Props) {
   return (
     <SpannedTableRow>
-      {count} time logs are outdated after {weekLimit} weeks.
+      {count} time logs are outdated after {weekLimit}{" "}
+      {pluralize(weekLimit, "week")}.
     </SpannedTableRow>
   );
 }
