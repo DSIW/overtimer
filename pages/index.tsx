0efc69f21be34eb22ecd1e218e5019bf3cf21f29
DSIW <dsiw@dsiw-it.de>
2022-06-29 22:15:59 +0200

    Clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import styles from "../styles/Home.module.css";
 import TimerApp from "../lib/interface/TimerApp";
 import React from "react";
-import createEmotionCache from "../lib/infrastructure/createEmotionCache";
-
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
 
 export default function Home() {
   return (
