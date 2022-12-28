e6837f7e18f99d9cd03131b6bd9e6e804268af12
Max <dsiw@dsiw-it.de>
2022-12-28 14:53:10 +0100

    Add tests

---
+++ lib/domain/analysis/TimeStatistic.test.ts
@@ -0,0 +1,41 @@
+import { parseISO } from "date-fns";
+import { withTime } from "../time-constants";
+import TimeStatistic from "./TimeStatistic";
+
+const DAY = parseISO("2022-08-01");
+
+let NINE = withTime(DAY, "09:00:00");
+let TEN = withTime(DAY, "10:00:00");
+let ELEVEN = withTime(DAY, "11:00:00");
+
+describe("TimeStatistic", () => {
+  describe("getTimeStatistics()", () => {
+    it("returns min, median and max", () => {
+      const timeStatistic = new TimeStatistic([
+        NINE,
+        TEN,
+        ELEVEN
+      ]);
+
+      expect(timeStatistic.getTimeStatistics()).toEqual({
+        min: NINE,
+        median: TEN,
+        max: ELEVEN
+      });
+    });
+
+    it("returns min, median and max of 2 same numbers", () => {
+      const timeStatistic = new TimeStatistic([
+        NINE,
+        ELEVEN,
+        ELEVEN
+      ]);
+
+      expect(timeStatistic.getTimeStatistics()).toEqual({
+        min: NINE,
+        median: ELEVEN,
+        max: ELEVEN
+      });
+    });
+  });
+});
