ed2b31674040ec6a26056f691c284e1f95b9e877
Max <dsiw@dsiw-it.de>
2022-12-31 12:48:55 +0100

    Remove test cases

--- lib/interface/Duration.test.tsx
+++ lib/interface/Duration.test.tsx
@@ -15,39 +15,15 @@ describe("<Duration />", () => {
     expect(await screen.findByText("ZERO")).toBeInTheDocument();
   });
 
-  it("renders seconds", async () => {
-    render(<Duration milliseconds={1 * SEC} />);
-
-    expect(await screen.findByText("1 s")).toBeInTheDocument();
-  });
-
-  it("renders minutes", async () => {
-    render(<Duration milliseconds={1 * MIN} />);
-
-    expect(await screen.findByText("1 m")).toBeInTheDocument();
-  });
-
   it("removes seconds if in minute range", async () => {
     render(<Duration milliseconds={1 * MIN + 1 * SEC} />);
 
     expect(await screen.findByText("1 m")).toBeInTheDocument();
   });
 
-  it("renders hours", async () => {
-    render(<Duration milliseconds={1 * HOUR} />);
-
-    expect(await screen.findByText("1 h")).toBeInTheDocument();
-  });
-
   it("renders hours and minutes", async () => {
     render(<Duration milliseconds={1 * HOUR + 1 * MIN} />);
 
     expect(await screen.findByText("1 h 1 m")).toBeInTheDocument();
-  });
-
-  it("renders days", async () => {
-    render(<Duration milliseconds={25 * HOUR + 1 * MIN} />);
-
-    expect(await screen.findByText("1 d 1 h 1 m")).toBeInTheDocument();
   });
 });
