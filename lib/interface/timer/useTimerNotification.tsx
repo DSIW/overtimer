cf1d9f15be79a434f8a9a8605b6fed4b9817b866
Max <dsiw@dsiw-it.de>
2023-02-25 18:28:41 +0100

    Request permissions on first page view

--- lib/interface/timer/useTimerNotification.tsx
+++ lib/interface/timer/useTimerNotification.tsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react";
 import {
   closeTimerNotification,
+  requestNotificationPermission,
   showTimerNotificationIfGranted,
 } from "../../infrastructure/Notification";
 import * as Sentry from "@sentry/browser";
 
 export default function useTimerNotification(isRunning: boolean, body: string) {
+  useEffect(() => {
+    requestNotificationPermission();
+  }, []);
+
   useEffect(() => {
     if (isRunning) {
       showTimerNotificationIfGranted("Timer is running", body).catch(
